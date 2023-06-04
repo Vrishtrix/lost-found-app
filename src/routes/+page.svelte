57daca2340a86af3c0906210d5528093c82b6bf0
Vrishin Patel <vrishin_patel@outlook.com>
2023-06-04 12:58:29 +0530

    feat: Create Login page (UI only)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { enhance } from '$app/forms';
+</script>
+
+<div class="grid grid-cols:2@sm min-h:100vh">
+	<div
+		class="bg:url('/images/login-bg.jpg') bg:center|no-repeat hidden block@sm"
+	/>
+
+	<div class="flex flex:col p:50 ai:center jc:space-between">
+		<div class="flex ai:center gap:20">
+			<img src="/images/logo.png" alt="logo" class="w:50" />
+			Lost Found.
+		</div>
+
+		<div class="flex flex:col w:full ai:center gap:50">
+			<h1>Welcome Back!</h1>
+
+			<form
+				method="POST"
+				class="flex flex:col gap:10 min-w:50% w:full w:auto@sm"
+				use:enhance
+			>
+				<label for="username" class="fg:gray-30">
+					Registrator Number:
+				</label>
+				<input
+					type="text"
+					name="username"
+					id="username"
+					placeholder="98ABC01234"
+					class="p:15 r:10 b:1|solid|gray-80"
+					required
+				/>
+				<br />
+				<label for="password" class="fg:gray-30"> Password: </label>
+				<input
+					type="password"
+					name="password"
+					id="password"
+					placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+					class="p:15 r:10 b:1|solid|gray-80"
+					required
+				/>
+
+				<button type="submit" class="my:15 py:15 bg:black fg:white r:10"
+					>Sign In</button
+				>
+			</form>
+		</div>
+
+		<div class="f:14 fg:gray-60">
+			&copy; Copyright 2023. All Rights Reserved.
+		</div>
+	</div>
+</div>
