59dfadb9e550171d395a0bfed7377c1374dbe1c4
Vrishin Patel <vrishin_patel@outlook.com>
2023-06-04 12:48:44 +0530

    feat: Implement prisma schema for auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,12 @@
+import { auth } from '$lib/server/lucia';
+
 import { renderIntoHTML } from '@master/css';
 import config from './master.css';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle = async ({ event, resolve }) => {
+	event.locals.auth = auth.handleRequest(event);
+
 	return await resolve(event, {
 		transformPageChunk: ({ html }) => renderIntoHTML(html, config),
 	});
